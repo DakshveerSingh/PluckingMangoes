238d1f9479f92448d5185d550d59d22dfae0c456
DakshveerSingh <70658130+DakshveerSingh@users.noreply.github.com>
2020-11-15 13:33:38 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -24,20 +24,27 @@ function setup() {
 
 	ground = new Ground(width/2,690,width,10);
 	tree = new Tree(600,690);
-	mango1 = new Mango(511,425,20);
+  mango1 = new Mango(511,425,20);
+  World.add(world,mango1);
   mango2 = new Mango(541,342,20);
-	mango3 = new Mango(641,327,20);
-	mango4 = new Mango(655,407,20);
-	mango5 = new Mango(573,383,20);
-	mango6 = new Mango(563,268,20);
+  World.add(world,mango2);
+  mango3 = new Mango(641,327,20);
+  World.add(world,mango3);
+  mango4 = new Mango(655,407,20);
+  World.add(world,mango4);
+  mango5 = new Mango(573,383,20);
+  World.add(world,mango5);
+  mango6 = new Mango(563,268,20);
+  World.add(world,mango6);
 
   var options = {
-    bodyA: stone,
+    bodyA: sling,
     pointB: {x: 5,y: 5},
     stiffness: 0.04,
     length: 10
   }
   sling = Constraint.create(options);
+  World.add(world,sling);
 
   stone = new Stone(40,40,40);
 
@@ -60,6 +67,7 @@ function draw() {
   mango4.display();
   mango5.display();
   mango6.display();
+  sling.display();
 }
 
 
