33034bcce918a9ed0fe5db3078d35611f25ab6f6
DakshveerSingh <70658130+DakshveerSingh@users.noreply.github.com>
2020-12-01 09:57:47 -0700

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -18,6 +18,10 @@ class SlingShot{
         this.sling.bodyA = null;
     }
 
+    attach(body){
+        this.sling.bodyA = body;
+    }
+
     display(){
         image(this.sling1,119,575);
         image(this.sling2,95,530);
